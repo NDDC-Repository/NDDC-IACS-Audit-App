7bf6d6391272b339946e08f4b6cabb07d62e2e21
Orchiid <joannatolofari@gmail.com>
2024-02-28 17:09:17 +0100

    Add Audit Completed

---
+++ NDDC-IACS-Audit/Pages/MainMenu/AuditControl/AddAudit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NDDC_IACS_Audit.Pages.MainMenu.AuditControl
+{
+    public class AddAuditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ NDDC-IACS-Audit/Pages/MainMenu/AuditControl/Audit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NDDC_IACS_Audit.Pages.MainMenu.AuditControl
+{
+    public class AuditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
