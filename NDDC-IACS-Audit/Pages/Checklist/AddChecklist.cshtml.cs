5c6676e882176ab0692f24d7770b021a08f087b5
Orchiid <joannatolofari@gmail.com>
2024-02-29 15:23:58 +0100

    Added Departments, Checklist and DeleteAudit

---
+++ NDDC-IACS-Audit/Pages/Checklist/AddChecklist.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NDDC_IACS_Audit.Pages.Checklist
+{
+    public class AddChecklistModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ NDDC-IACS-Audit/Pages/Checklist/DeleteChecklist.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NDDC_IACS_Audit.Pages.Checklist
+{
+    public class DeleteChecklistModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
